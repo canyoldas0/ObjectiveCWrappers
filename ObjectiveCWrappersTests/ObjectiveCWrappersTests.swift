a7f3860a59dfcfa3ea6f56e1367c2e19a8e3263d
Can Yoldas <can.yoldas0@outlook.com>
2021-12-05 10:50:00 +0300

    Initial Commit

---
+++ ObjectiveCWrappersTests/ObjectiveCWrappersTests.swift
@@ -0,0 +1,33 @@
+//
+//  ObjectiveCWrappersTests.swift
+//  ObjectiveCWrappersTests
+//
+//  Created by Can Yoldas on 5.12.2021.
+//
+
+import XCTest
+@testable import ObjectiveCWrappers
+
+class ObjectiveCWrappersTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
